cc51983d5756e6167b5f172bf9cf6c4c4f4375e1
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-06-15 15:14:26 -0500

    endgame function

--- assets/script.js
+++ assets/script.js
@@ -89,16 +89,75 @@ var fight = function (enemyName) {
     }
     }}
 
+var startGame = function () {   
+    
+    //reset player stats
 
-for (var i=0; i<enemyNames.length; i++) {
+    playerHealth=100;
+    playerAttack=10;
+    playerMoney=10;
 
-    var pickedEnemyName = enemyNames[i];
 
-    enemyHealth = 50;
+    for (var i=0; i<enemyNames.length; i++) {
 
-    fight(enemyNames[i]);
+        if (playerHealth>0) {
 
-}
+            window.alert("Welcome to Robot Gladiators! Round " + ( i +1));
+    
+
+        var pickedEnemyName = enemyNames[i];
+
+        enemyHealth = 50;
+
+        fight(pickedEnemyName);
+
+    }
+        else {
+
+            window.alert("You have lost your robot in battle! Game Over!");
+
+            break;
+        }
+
+    }
+
+    endGame();
+
+
+};
+
+//start the game
+
+
+var endGame=function() {
+
+    if (playerHealth>0){
+
+        window.alert("Great job, you've survived the game! You now have a score of" + playerMoney + " . ");
+    }
+
+    else {
+        
+        window.alert("The game has ended. Let's see how you did!");
+    }
+
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+
+        startGame();
+    }
+    else {
+
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+
+
+};
+
+startGame();
+
+
 
 //Game states
 
